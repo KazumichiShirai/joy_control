5c36629edd8ebece64a4c4e41adebc097847598b
KazumichiShirai <shirai@whill.jp>
2016-04-13 10:42:10 +0900

    Change speed by pushing buttom. Support new spec.

--- joy_control.py
+++ joy_control.py
@@ -13,9 +13,28 @@ spi = spidev.SpiDev()
 spi.open(0, 0)
 spi.max_speed_hz = 1000000
 spi.mode = 0b00
-set_speed = [0x00]
-right_motor = [0x00]
-left_motor = [0x00]
+protocol_sign = 0xaf
+set_speed = 0x00
+off_control = 0x01
+dummy = 0x00
+line = 0x0a
+right_motor = 0x00
+left_motor = 0x00
+alpha = 0.5
+
+def set_motor_value(x, y):
+        global right_motor
+        global left_motor
+        right_motor = int((-y  - x * alpha)* limit)
+        left_motor = int((y - x * alpha) * limit)
+        if right_motor > 100:
+            right_motor = 100
+        elif right_motor < -100:
+            right_motor = -100
+        if left_motor > 100:
+            left_motor = 100
+        elif left_motor < -100:
+            left_motor = -100
 
 print (pygame.version.ver)
 pygame.init()
@@ -27,30 +46,45 @@ try:
 except pygame.error:
         print ('No Joystick')
 pygame.display.init()
-
+command = [protocol_sign, set_speed, right_motor, left_motor, line]
+msg = ''
+limit = 50
 while 1:
         e = pygame.event.poll() # イベントチェック
         # Joystick関連のイベントチェック
         if e.type == pygame.locals.JOYAXISMOTION: # 7
             x , y = j.get_axis(0), j.get_axis(1)
-            #print ('x and y : ',  str(x) +' , '+ str(y))
-            right_motor[0] = int(x * 100)
-            left_motor[0] = int(y * 100)
-            spi.xfer(set_speed)
-            spi.xfer(right_motor)
-            spi.xfer(left_motor)
+            set_motor_value(x,y)
+            command = [protocol_sign, set_speed, right_motor, left_motor, line]
+            r = spi.xfer(command) #戻り値は配列
             print x
             print y
             pygame.event.pump()
-        elif e.type == pygame.locals.JOYHATMOTION: # 9
-            print ('hat motion')
-            time.sleep(1)
-            spi.close()
-            pygame.quit()
-            break
+        elif e.type == pygame.locals.JOYBUTTONDOWN: # 10
+                if e.button == 2:
+                        print ('terminate')
+                        command = [protocol_sign, off_control, dummy, dummy, line]
+                        time.sleep(1)
+                        spi.close()
+                        pygame.quit()
+                        break
+                if e.button == 1:
+                        limit = 100
+                        set_motor_value(x,y)
+                        command = [protocol_sign, set_speed, right_motor, left_motor, line]
+                        print limit
+        elif e.type == pygame.locals.JOYBUTTONUP:
+                if e.button == 1:
+                        limit = 50
+                        set_motor_value(x,y)
+                        command = [protocol_sign, set_speed, right_motor, left_motor, line]
+                        print limit
         elif e.type == pygame.locals.NOEVENT:
-            spi.xfer(set_speed)
-            spi.xfer(right_motor)
-            spi.xfer(left_motor)
-            time.sleep(0.05)
+            r = spi.xfer(command) #戻り値は配列
+            time.sleep(0.02)
+        # if r[0][0] != 0x00:
+        #     msg += chr(r[0][0]) + chr(r[1][0]) + chr(r[2][0])
+        #     if [0x0a] in r:
+        #         print msg
+        #         msg = ''
 # end of file
